87050b829b8fc736e37275dc0be848e1b21cba45
Kamil Żłobiński <kamil.zlobinski@paxillus.pl>
2021-04-24 01:09:08 +0200

    add raconteur service

---
+++ PylaeIT.SignalRaconteur.ConsoleApp/RaconteurService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace PylaeIT.SignalRaconteur.ConsoleApp
+{
+    public class RaconteurService
+    {
+        private readonly HubConnection _connection;
+
+        public event Action<string> MessageReceived; 
+
+        public RaconteurService(HubConnection connection)
+        {
+            _connection = connection;
+
+            _connection.On<string>("ReceiveMessage", (message) => MessageReceived?.Invoke(message));
+        }
+
+        public async Task Connect()
+        {
+            await _connection.StartAsync();
+        }
+
+        public async Task SendMessage(string mesage)
+        {
+            await _connection.SendAsync("SendMessage", mesage);
+        }
+    }
+}
