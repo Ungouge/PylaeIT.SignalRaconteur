6e964026aa9c063b5ba49d0fc2eb0392b252d95c
Kamil Żłobiński <kamil.zlobinski@paxillus.pl>
2021-04-24 00:58:02 +0200

    add RaconteurHub

---
+++ PylaeIT.SignalRaconteur.RaconteurHub/RaconteurHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace PylaeIT.SignalRaconteur.RaconteurHub
+{
+    public class RaconteurHub : Hub
+    {
+        public async Task  SendMessage(string mesage) 
+        {
+            await Clients.All.SendAsync("ReceiveMessage", mesage);
+        }
+    }
+}
