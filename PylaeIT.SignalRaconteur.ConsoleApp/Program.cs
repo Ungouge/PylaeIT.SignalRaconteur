7dce45d9fd225c9eab3ab1c1779f764393724075
Kamil Żłobiński <kamil.zlobinski@paxillus.pl>
2021-04-24 02:16:04 +0200

    add app guts

--- PylaeIT.SignalRaconteur.ConsoleApp/Program.cs
+++ PylaeIT.SignalRaconteur.ConsoleApp/Program.cs
@@ -7,7 +7,32 @@ namespace PylaeIT.SignalRaconteur.ConsoleApp
     {
         static void Main(string[] args)
         {
-            HubConnection connection = new HubConnectionBuilder().WithUrl("https://localhost:5001").Build();
+            var connection = new HubConnectionBuilder().WithUrl("https://localhost:5001/raconteur").Build();
+
+            var service = new RaconteurService(connection);
+
+            service.MessageReceived += (string message) => Console.WriteLine(message);
+
+            service.Connect().ContinueWith(task =>
+            {
+                if (task.Exception != null)
+                {
+                    Console.WriteLine("Connection Error");
+                }
+            });
+
+            while (true)
+            {
+                var text = Console.ReadLine();
+
+                if (text.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                service.SendMessage(text);
+            }
+
 
             Console.WriteLine("Hello World!");
         }
